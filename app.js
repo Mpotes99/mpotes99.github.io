490cb07f95de2120a25b5388efcb712067ebf0e7
Mauricio Potes <potesmauricio99@gmail.com>
2023-11-27 20:29:08 -0300

    act

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(error => {
-            console.error('Error al registrar el Service Worker:', error);
-        });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const noteForm = document.getElementById('noteForm');
     const noteContentInput = document.getElementById('noteContent');
     const noteList = document.getElementById('noteList');
     const downloadButton = document.getElementById('downloadButton');
     const deleteAllButton = document.getElementById('deleteAllButton');
+    const mobileDownloadBanner = document.getElementById('mobileDownloadBanner');
+    const downloadAppButton = document.getElementById('downloadAppButton');
 
     loadNotes();
 
@@ -39,92 +31,109 @@ document.addEventListener('DOMContentLoaded', () => {
         deleteAllNotes();
     });
 
-    function loadNotes() {
-        const notes = getNotes();
+    if (isMobile() && !isAppInstalled()) {
+        mobileDownloadBanner.style.display = 'block';
 
-        noteList.innerHTML = '';
-
-        notes.forEach((note, index) => {
-            const li = document.createElement('li');
-            li.textContent = `${index + 1}. ${note}`;
-            li.classList.add('note-item');
-
-            const botonContainer = document.createElement('div');
-            botonContainer.classList.add('boton-container');
-
-            const editButton = document.createElement('button');
-            editButton.textContent = 'Editar';
-            editButton.addEventListener('click', () => editNote(index));
-            editButton.classList.add('boton-item');
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Eliminar';
-            deleteButton.addEventListener('click', () => deleteNote(index, li));
-            deleteButton.classList.add('boton-eliminar');
-
-            botonContainer.appendChild(editButton);
-            botonContainer.appendChild(deleteButton);
-
-            li.appendChild(botonContainer);
-
-            noteList.appendChild(li);
+        downloadAppButton.addEventListener('click', () => {
+            alert('La aplicación se agregará a tu pantalla de inicio.');
+            mobileDownloadBanner.style.display = 'none';
         });
-    }
-
-    function saveNote(content) {
-        const notes = getNotes();
-        notes.push(content);
-        localStorage.setItem('notes', JSON.stringify(notes));
-    }
-
-    function getNotes() {
-        return JSON.parse(localStorage.getItem('notes')) || [];
-    }
-
-    function editNote(index) {
-        const notes = getNotes();
-        const editedNote = prompt('Editar tarea:', notes[index]);
-
-        if (editedNote !== null) {
-            notes[index] = editedNote.trim();
-            localStorage.setItem('notes', JSON.stringify(notes));
-            loadNotes();
-        }
-    }
-
-    function deleteNote(index, element) {
-        const notes = getNotes();
-        notes.splice(index, 1);
-        localStorage.setItem('notes', JSON.stringify(notes));
-
-        element.classList.add('delete-animation');
-
-        element.addEventListener('animationend', () => {
-            loadNotes();
-        });
-    }
-
-    function downloadNotes() {
-        const notes = getNotes();
-        const notesText = notes.join('\n');
-        const blob = new Blob([notesText], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'notas.txt';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-    }
-
-    function deleteAllNotes() {
-        const confirmDelete = confirm('¿Estás seguro de que deseas borrar todas las notas?');
-
-        if (confirmDelete) {
-            localStorage.removeItem('notes');
-            loadNotes();
-        }
     }
 });
+
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function isAppInstalled() {
+    return false;
+}
+
+function loadNotes() {
+    const notes = getNotes();
+
+    noteList.innerHTML = '';
+
+    notes.forEach((note, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${index + 1}. ${note}`;
+        li.classList.add('note-item');
+
+        const botonContainer = document.createElement('div');
+        botonContainer.classList.add('boton-container');
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Editar';
+        editButton.addEventListener('click', () => editNote(index));
+        editButton.classList.add('boton-item');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Eliminar';
+        deleteButton.addEventListener('click', () => deleteNote(index, li));
+        deleteButton.classList.add('boton-eliminar');
+
+        botonContainer.appendChild(editButton);
+        botonContainer.appendChild(deleteButton);
+
+        li.appendChild(botonContainer);
+
+        noteList.appendChild(li);
+    });
+}
+
+function saveNote(content) {
+    const notes = getNotes();
+    notes.push(content);
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function getNotes() {
+    return JSON.parse(localStorage.getItem('notes')) || [];
+}
+
+function editNote(index) {
+    const notes = getNotes();
+    const editedNote = prompt('Editar tarea:', notes[index]);
+
+    if (editedNote !== null) {
+        notes[index] = editedNote.trim();
+        localStorage.setItem('notes', JSON.stringify(notes));
+        loadNotes();
+    }
+}
+
+function deleteNote(index, element) {
+    const notes = getNotes();
+    notes.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+
+    element.classList.add('delete-animation');
+
+    element.addEventListener('animationend', () => {
+        loadNotes();
+    });
+}
+
+function downloadNotes() {
+    const notes = getNotes();
+    const notesText = notes.join('\n');
+    const blob = new Blob([notesText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'notas.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+function deleteAllNotes() {
+    const confirmDelete = confirm('¿Estás seguro de que deseas borrar todas las notas?');
+
+    if (confirmDelete) {
+        localStorage.removeItem('notes');
+        loadNotes();
+    }
+}
