37a6b927b643500dee0e7ae4997fc68e7f518884
Mauricio Potes <potesmauricio99@gmail.com>
2023-11-27 18:34:54 -0300

    act

--- app.js
+++ app.js
@@ -35,20 +35,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Función para cargar notas desde localStorage y mostrarlas en la interfaz
     function loadNotes() {
-        // Obtener notas desde localStorage
         const notes = getNotes();
 
-        // Limpiar la lista antes de mostrar las notas
         noteList.innerHTML = '';
 
-        // Mostrar cada nota en la lista
         notes.forEach((note, index) => {
             const li = document.createElement('li');
             li.textContent = `${index + 1}. ${note}`;
 
-            // Agregar botones de editar y eliminar
             const editButton = document.createElement('button');
             editButton.textContent = 'Editar';
             editButton.addEventListener('click', () => editNote(index));
@@ -64,19 +59,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Función para guardar una nueva nota en localStorage
     function saveNote(content) {
         const notes = getNotes();
         notes.push(content);
         localStorage.setItem('notes', JSON.stringify(notes));
     }
 
-    // Función para obtener todas las notas desde localStorage
     function getNotes() {
         return JSON.parse(localStorage.getItem('notes')) || [];
     }
 
-    // Función para editar una nota existente
     function editNote(index) {
         const notes = getNotes();
         const editedNote = prompt('Editar nota:', notes[index]);
@@ -88,7 +80,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Función para eliminar una nota existente
     function deleteNote(index) {
         const notes = getNotes();
         notes.splice(index, 1);
